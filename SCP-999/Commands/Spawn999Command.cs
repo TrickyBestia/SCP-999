efda03870dbc683dfab9111142e1ef4fba4b2bf9
TrickyBestia <thewonderfulcookie@gmail.com>
2020-12-09 09:23:20 +0300

    Wrote some code

---
+++ SCP-999/Commands/Spawn999Command.cs
@@ -0,0 +1,38 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using System;
+
+namespace SCP_999.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class Spawn999Command : ICommand
+    {
+        public string Command { get; } = "spawn999";
+
+        public string[] Aliases { get; } = new string[0];
+
+        public string Description { get; } = "Turns player with specified id into SCP-999";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            int id;
+            if (arguments.Count != 1 || !int.TryParse(arguments.At(0), out id))
+            {
+                response = "Incorrect arguments.";
+                return false;
+            }
+            else if (Scp999Manager.IsScp999(Player.Get(id)))
+            {
+                response = "Player is already SCP-999.";
+                return false;
+            }
+            else
+            {
+                var player = Player.Get(id);
+                Scp999Manager.MakeScp999(player);
+                response = $"Ok, {player.Nickname} is SCP-999 now.";
+                return true;
+            }
+        }
+    }
+}
---
+++ SCP-999/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Handlers = Exiled.Events.Handlers;
+
+namespace SCP_999
+{
+    public class Plugin : Plugin<PluginConfig>
+    {
+        public override void OnEnabled()
+        {
+            Handlers.Server.RestartingRound += OnServerRestartingRound;
+            Handlers.Player.ChangingRole += OnPlayerChangingRole;
+
+            base.OnEnabled();
+        }
+        private void OnPlayerChangingRole(ChangingRoleEventArgs ev)
+        {
+            if (Scp999Manager.IsScp999(ev.Player))
+                Scp999Manager.UnMakeScp999(ev.Player);
+        }
+        private void OnServerRestartingRound()
+        {
+            Scp999Manager.Clear();
+        }
+    }
+}
