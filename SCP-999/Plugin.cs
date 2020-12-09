e4920aea0d6905e5c86141b991893e1ecc14a1f3
TrickyBestia <thewonderfulcookie@gmail.com>
2020-12-09 11:42:20 +0300

    Fixed plugin name in config

--- SCP-999/Plugin.cs
+++ SCP-999/Plugin.cs
@@ -1,11 +1,20 @@
-﻿using Exiled.API.Features;
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
 using Exiled.Events.EventArgs;
+using System;
 using Handlers = Exiled.Events.Handlers;
 
 namespace SCP_999
 {
     public class Plugin : Plugin<PluginConfig>
     {
+        public override string Author { get; } = "TrickyBestia";
+        public override string Name { get; } = "SCP-999";
+        public override string Prefix { get; } = "SCP-999";
+        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 18);
+        public override PluginPriority Priority { get; } = PluginPriority.Default;
+        public override Version Version => Assembly.GetName().Version;
+
         public override void OnEnabled()
         {
             Handlers.Server.RestartingRound += OnServerRestartingRound;
